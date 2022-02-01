8ce251aa0e834855b289e177bc227aeb409de8ff
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-02-01 07:23:42 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,14 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fetch(fla + 'donasi')).buffer(), `
-┌〔 Donasi • Emoney 〕
-├ PULSA : 085742344873
-├ OVO & DANA : 085742344873
+┌〔 Donasi • Pulsa 〕
+├ Smartfren : 088233832771
+├ Tri : 089612698583
+└────
+┌〔 Donasi • Non Pulsa 〕
+├ Dana : 088233832771
+├ QR : https://a.top4top.io/p_2212zqtaa0.jpg
+├ Donasi Hubungi : wa.me//6288233832771
+├ MAKASIH YANG SUDAH DONASI
 └────
 `.trim(), footer, 'Owner', '.owner')
 handler.help = ['donasi']
