e4c09a355f0af62eceb0c5e93fa23926826d8d5a
games-wabot <97183746+Botwa021@users.noreply.github.com>
2022-02-01 16:02:12 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 function handler(m) {
-  m.reply('Ada perlu apa kak cari owner?, harap tidak spam ya.')
+  m.reply('Ada perlu apa kak cari owner?, chat *P* tidak akan di balas')
   this.sendContact(m.chat, '6288233832771@s.whatsapp.net', 'Mursid (Owner)', m)
 }
 handler.help = ['owner', 'creator']
